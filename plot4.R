7dd5a445fd1e3bbda1b7d4a51f2c4c52883c7685
23f3003822 <23f3003822@ds.study.iitm.ac.in>
2026-04-17 21:38:36 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,6 @@
+png("plot4.png", 480, 480)
+
+
 data <- read.table("household_power_consumption.txt",
                    header=TRUE,
                    sep=";",
@@ -12,3 +15,31 @@ data$Datetime <- strptime(
   paste(data$Date, data$Time),
   format="%Y-%m-%d %H:%M:%S"
 )
+
+
+# data code here
+
+par(mfrow=c(2,2))
+
+plot(data$Datetime, data$Global_active_power,
+     type="l", xlab="", ylab="Global Active Power")
+
+plot(data$Datetime, data$Voltage,
+     type="l", xlab="datetime", ylab="Voltage")
+
+plot(data$Datetime, data$Sub_metering_1,
+     type="l", xlab="", ylab="Energy sub metering")
+
+lines(data$Datetime, data$Sub_metering_2, col="red")
+lines(data$Datetime, data$Sub_metering_3, col="blue")
+
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=1,
+       bty="n")
+
+plot(data$Datetime, data$Global_reactive_power,
+     type="l", xlab="datetime", ylab="Global Reactive Power")
+
+dev.off()
