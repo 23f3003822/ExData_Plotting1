147950bf93c7a576d55e2fa3a7e8524def72aede
23f3003822 <23f3003822@ds.study.iitm.ac.in>
2026-04-17 21:38:18 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,7 @@
+png("plot3.png", 480, 480)
+
+
+
 data <- read.table("household_power_consumption.txt",
                    header=TRUE,
                    sep=";",
@@ -12,3 +16,19 @@ data$Datetime <- strptime(
   paste(data$Date, data$Time),
   format="%Y-%m-%d %H:%M:%S"
 )
+
+
+# data code here
+
+plot(data$Datetime, data$Sub_metering_1,
+     type="l", ylab="Energy sub metering", xlab="")
+
+lines(data$Datetime, data$Sub_metering_2, col="red")
+lines(data$Datetime, data$Sub_metering_3, col="blue")
+
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=1)
+
+dev.off()
